0f3a11e67e2c9bead1124da067a784b98d9275b5
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-04-28 14:37:14 +0100

    update natspec comments

--- contracts/BadgeFactory.sol
+++ contracts/BadgeFactory.sol
@@ -1,5 +1,12 @@
 pragma solidity 0.6.6;
 
+/// @title Non-transferable Badges for Maker Ecosystem Activity, issue #537
+/// @author Nazzareno Massari, Scott Herren, Bryan Flynn
+/// @notice BadgeFactory to manage Templates and Badges
+/// @dev see https://github.com/makerdao/community/issues/537
+/// @dev All function calls are currently implemented without side effecs through TDD approach
+/// @dev OpenZeppelin library is used for secure contract development
+
 import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721Holder.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
--- contracts/InsigniaDAO.sol
+++ contracts/InsigniaDAO.sol
@@ -1,5 +1,12 @@
 pragma solidity 0.6.6;
 
+/// @title Non-transferable Badges for Maker Ecosystem Activity, issue #537
+/// @author Nazzareno Massari, Scott Herren, Bryan Flynn
+/// @notice InsigniaDAO to check for activities on maker system and activate badges
+/// @dev see https://github.com/makerdao/community/issues/537
+/// @dev All function calls are currently implemented without side effecs through TDD approach
+/// @dev OpenZeppelin library is used for secure contract development
+
 contract InsigniaDAO {
 
 
