a9a2375cbff787d81198a4623734f04df5981e63
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-05-01 16:11:28 +0100

    update tests

--- test/BadgeFactory.test.js
+++ test/BadgeFactory.test.js
@@ -4,7 +4,6 @@ var BadgeFactory = artifacts.require('./BadgeFactory')
 contract('BadgeFactory', function(accounts) {
 
   const owner = accounts[0]
-  const minter = accounts[1]
   const random = accounts[3]
 
   const DEFAULT_ADMIN_ROLE = "0x00"
@@ -21,7 +20,6 @@ contract('BadgeFactory', function(accounts) {
   // Before Each
   beforeEach(async () => {
     instance = await BadgeFactory.new()
-    await instance.addMinter(minter, {from:owner})
   })
 
   // Check that the owner is set as the deploying address
@@ -32,11 +30,6 @@ contract('BadgeFactory', function(accounts) {
       it("OWNER should be set to the deploying address", async() => {
           const ownerAddress = await instance.owner()
           assert.equal(ownerAddress, owner, "the deploying address should be the owner")
-      })
-
-      it("Owner should be set as Default Admin when the contract is created", async() => {
-          const adminAdded = await instance.isAdmin(owner)
-          assert.isTrue(adminAdded, "the owner should be set as default admin")
       })
 
       it("Owner should be the only admin when the contract is created", async() => {
@@ -54,7 +47,7 @@ contract('BadgeFactory', function(accounts) {
       describe("createTemplate()", async () => {
 
         it("minters should be able to create a template", async () => {
-          await instance.createTemplate(name, description, image, limit, {from:minter})
+          await instance.createTemplate(name, description, image, limit, {from:owner})
           const result = await instance.getTemplate(templateId, {from:random})
           assert.equal(result[0], name, "the name of the created template does not match the expected value")
           assert.equal(result[1], description, "the description of the created template does not match the expected value")
@@ -65,7 +58,7 @@ contract('BadgeFactory', function(accounts) {
         })
 
         it("should emit the appropriate event when a template is created", async () => {
-          const result = await instance.createTemplate(name, description, image, limit, {from:minter})
+          const result = await instance.createTemplate(name, description, image, limit, {from:owner})
           assert.equal(result.logs[0].event, "NewTemplate", "NewTemplate event not emitted, check createTemplate method")
         })
 
@@ -81,21 +74,21 @@ contract('BadgeFactory', function(accounts) {
       describe("destroyTemplate()", async () => {
 
         it("minters should be able to destroy a template", async () => {
-          await instance.createTemplate(name, description, image, limit, {from:minter})
-          await instance.destroyTemplate(templateId, {from:minter})
+          await instance.createTemplate(name, description, image, limit, {from:owner})
+          await instance.destroyTemplate(templateId, {from:owner})
           await catchRevert(instance.getTemplate(templateId, {from:random}))
           const result = await instance.getTemplatesCount({from:random})
           assert.equal(result, 0, "the number of templates does not match the expected value")
         })
 
         it("should emit the appropriate event when a template is destroyed", async () => {
-          await instance.createTemplate(name, description, image, limit, {from:minter})
-          const result = await instance.destroyTemplate(templateId, {from:minter})
+          await instance.createTemplate(name, description, image, limit, {from:owner})
+          const result = await instance.destroyTemplate(templateId, {from:owner})
           assert.equal(result.logs[0].event, "TemplateDestroyed", "TemplateDestroyed event not emitted, check destroyTemplate method")
         })
 
         it("random address should not be able to destroy a template", async () => {
-          await instance.createTemplate(name, description, image, limit, {from:minter})
+          await instance.createTemplate(name, description, image, limit, {from:owner})
           await catchRevert(instance.destroyTemplate(templateId, {from:random}))
         })
 
