3fcf6da19199b284223e2063360abc68396e598d
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-05-03 00:25:14 +0100

    update BadgeRoles.test.js

--- test/BadgeRoles.test.js
+++ test/BadgeRoles.test.js
@@ -27,11 +27,6 @@ contract('BadgeRoles', function(accounts) {
           assert.equal(ownerAddress, owner, "the deploying address should be the owner")
       })
 
-      it("Owner should be set as Default Admin when the contract is created", async() => {
-          const adminAdded = await instance.isAdmin(owner)
-          assert.isTrue(adminAdded, "the owner should be set as default admin")
-      })
-
       it("Owner should be the only admin when the contract is created", async() => {
           const admins = await instance.getRoleMemberCount(DEFAULT_ADMIN_ROLE)
           assert.equal(admins, "1", "the owner should be the only admin")
@@ -42,49 +37,47 @@ contract('BadgeRoles', function(accounts) {
 
   describe("Functions", () => {
 
-    // Check addTemplater() for success when an admin is adding a new templater
-    // Check addTemplater() for sucessfully emit event when the templater is added
-    // Check addTemplater() for failure when a random address try to add a new templater
-    describe("addTemplater()", async () => {
+    // Check pause() for success when a pauser is pausing all the functions
+    // Check pause() for sucessfully emit event when the functions are paused
+    // Check pause() for failure when a random address try to pause all the functions
+    describe("pause()", async () => {
 
-      it("admins should be able to add a templater", async () => {
-        await instance.addTemplater(templater, {from:owner})
-        const templaterAdded = await instance.isTemplater(templater, {from:random})
-        assert.isTrue(templaterAdded, "only admin can add new templaters")
+      it("pauser should be able to pause all the functions", async () => {
+        await instance.pause({from:owner})
+        await catchRevert(instance.pause({from:owner}))
       })
 
-      it("should emit the appropriate event when a templater is added", async () => {
-        const result = await instance.addTemplater(templater, {from:owner})
-        assert.equal(result.logs[0].event, "RoleGranted", "RoleGranted event not emitted, check addTemplater method")
+      it("should emit the appropriate event when the functions are paused", async () => {
+        const result = await instance.pause({from:owner})
+        assert.equal(result.logs[0].event, "Paused", "Paused event not emitted, check pause method")
       })
 
-      it("random address should not be able to add a templater", async () => {
-        await catchRevert(instance.addTemplater(templater, {from:random}))
+      it("random address should not be able to pause all functions", async () => {
+        await catchRevert(instance.pause({from:random}))
       })
     })
 
-    // Check removeTemplater() for success when an amdin is removing a templater
-    // Check remvoveTemplater() for sucessfully emit event when the templater is removed
-    // Check removeTemplater() for failure when a random address try to remove a templater
-    describe("removeTemplater()", async () => {
+    // Check unpause() for success when an amdin is unpausing all the functions
+    // Check unpause() for sucessfully emit event when the functions are unpaused
+    // Check unpause() for failure when a random address try to unpause all the functions
+    describe("unpause()", async () => {
 
-      it("admins should be able to remove a templater", async () => {
-        await instance.addTemplater(templater, {from:owner})
-        await instance.removeTemplater(templater, {from:owner})
-        const templaterRemoved = await instance.isTemplater(templater, {from:random})
-        assert.isFalse(templaterRemoved, "only admin can remove templater")
+      it("admins should be able to unpause all the functions", async () => {
+        await instance.pause({from:owner})
+        await instance.unpause({from:owner})
+        await catchRevert(instance.unpause({from:owner}))
       })
 
-      it("should emit the appropriate event when a templater is removed", async () => {
-        await instance.addTemplater(templater, {from:owner})
-        const result = await instance.removeTemplater(templater, {from:owner})
-        assert.equal(result.logs[0].event, "RoleRevoked", "RoleRevoked event not emitted, check removeTemplater method")
+      it("should emit the appropriate event when all functions are unpaused", async () => {
+        await instance.pause({from:owner})
+        const result = await instance.unpause({from:owner})
+        assert.equal(result.logs[0].event, "Unpaused", "Unpaused event not emitted, check pause method")
       })
 
-      it("random address should not be able to remove a templater", async () => {
-        await instance.addTemplater(templater, {from:owner})
-        await catchRevert(instance.removeTemplater(templater, {from:random}))
-      })	
+      it("random address should not be able to unpause all the functions", async () => {
+        await instance.pause({from:owner})
+        await catchRevert(instance.unpause({from:random}))
+      })
     })
   })
 })
