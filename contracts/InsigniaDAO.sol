fb472e83da8494d156abb27a836670252a3a7432
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-05-03 17:08:28 +0100

    add access management for pauser

--- contracts/InsigniaDAO.sol
+++ contracts/InsigniaDAO.sol
@@ -8,6 +8,7 @@ pragma solidity 0.6.6;
 /// @dev OpenZeppelin library is used for secure contract development
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
@@ -20,11 +21,13 @@ interface PotLike {
 
 }
 
-contract InsigniaDAO is Ownable, Pausable {
+contract InsigniaDAO is Ownable, AccessControl, Pausable {
 
   using SafeMath for uint256;
   using Address for address;
   using EnumerableSet for EnumerableSet.AddressSet;
+
+  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
   EnumerableSet.AddressSet private redeemers;
 
@@ -39,24 +42,27 @@ contract InsigniaDAO is Ownable, Pausable {
 
   uint constant WAD = 10 ** 18;
 
-  function wmul(uint x, uint y) internal pure returns (uint z) {
+  function wmul(uint x, uint y) internal view whenNotPaused returns (uint z) {
           // always rounds down
           z = x.mul(y) / WAD;
   }
 
   constructor() public {
+        _setupRole(DEFAULT_ADMIN_ROLE, owner());
+
+        _setupRole(PAUSER_ROLE, owner());
 
         // MCD_POT Kovan Address https://changelog.makerdao.com/releases/kovan/1.0.5/contracts.json
 			  pot = PotLike(0xEA190DBDC7adF265260ec4dA6e9675Fd4f5A78bb);
 	}
 
-  function balance(address guy) internal view returns (uint256) {
+  function balance(address guy) internal view whenNotPaused returns (uint256) {
     uint256 slice = pot.pie(guy);
     uint256 chi = pot.chi();
     return wmul(slice, chi);
   }
 
-  function dsrChallenge() public returns (bool) {
+  function dsrChallenge() public whenNotPaused returns (bool) {
     uint256 interest = balance(msg.sender);
     require(interest == 1, "The caller has not accrued 1 Dai interest");
     redeemers.add(msg.sender);
@@ -64,7 +70,7 @@ contract InsigniaDAO is Ownable, Pausable {
     return true;
   }
 
-  function verify(address guy) public view returns (bool) {
+  function verify(address guy) public view whenNotPaused returns (bool) {
     require(redeemers.contains(guy) == true, "The address is not a redeemer");
     return true;
   }
