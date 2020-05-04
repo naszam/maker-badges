d3f08e00eb1f314e6a032d621fe6e4104be7315e
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-05-04 15:59:43 +0100

    update migrations, link InsigniaDAO to BadgeFactory

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var InsigniaDAO = artifacts.require("InsigniaDAO");
+var BadgeFactory = artifacts.require("BadgeFactory");
 
 module.exports = function(deployer) { 
-	deployer.deploy(InsigniaDAO); 
+	deployer.deploy(InsigniaDAO).then(function() {
+		return deployer.deploy(BadgeFactory, InsigniaDAO.address);
+	});
 };
