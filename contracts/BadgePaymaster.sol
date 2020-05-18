fccd28a4a9af25e2feb5a0c3d3c07745c67b4f4f
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-05-18 20:04:46 +0100

    update BadgePaymaster.sol

--- contracts/BadgePaymaster.sol
+++ contracts/BadgePaymaster.sol
@@ -1,3 +1,4 @@
+/// SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.6.8;
 pragma experimental ABIEncoderV2;
 
@@ -8,61 +9,48 @@ pragma experimental ABIEncoderV2;
 /// @dev All function calls are currently implemented without side effecs through TDD approach
 /// @dev OpenZeppelin v3.0 library is used for secure contract development
 
-import "@opengsn/gsn/contracts/utils/GSNTypes.sol";
 import "@opengsn/gsn/contracts/BasePaymaster.sol";
 
 contract BadgePaymaster is BasePaymaster {
-
-using GSNTypes for GSNTypes.RelayRequest;
-using GSNTypes for GSNTypes.GasData;
 
 constructor() public {
 }
 
 /// Functions
 
-//return the payer of this request.
-// for account-based target, this is the target account.
-function getPayer(GSNTypes.RelayRequest calldata relayRequest) external pure returns (address) {
-    return relayRequest.target;
+mapping(address => bool) public whitelist;
+
+function addToWhitelist(address _address) onlyOwner external {
+    whitelist[_address] = true;
 }
 
 function acceptRelayedCall(
     GSNTypes.RelayRequest calldata relayRequest,
-    bytes calldata approvalData,
-    uint256 maxPossibleGas
+    bytes calldata,
+    uint256
 )
-external
 override
+external
 view
-returns (bytes memory context) {
-
-          (approvalData);
-          address payer = this.getPayer(relayRequest);
-          uint256 ethMaxCharge = relayHub.calculateCharge(maxPossibleGas, relayRequest.gasData);
-
-          return abi.encode(payer);
-
+returns (bytes memory){
+    require(whitelist[relayRequest.relayData.senderAddress], "not whitelisted");
+    return "";
 }
 
-function preRelayedCall(bytes calldata context) external override relayHubOnly returns (bytes32) {
-
-        return bytes32(0);
+function preRelayedCall(bytes calldata) override external returns (bytes32){
+    return 0;
 }
 
 function postRelayedCall(
-    bytes calldata context,
-    bool success,
-    bytes32 preRetVal,
-    uint256 gasUseWithoutPost,
-    GSNTypes.GasData calldata gasData
-) external override relayHubOnly {
-    (success, preRetVal);
+    bytes calldata,
+    bool,
+    bytes32,
+    uint256,
+    GSNTypes.GasData calldata
+)
+override
+external {
 
-    uint256 ethActualCharge;
-    uint256 justPost;
-
-    relayHub.depositFor{value: ethActualCharge}(address(this));
 }
 
 }
