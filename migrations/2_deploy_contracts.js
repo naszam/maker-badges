fa22a6543994c0780d7a6048d4505194aaf5d2ad
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-07-05 21:36:47 +0100

    update migrations, add kovan maker addresses of pot, chief and flipper

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var MakerBadges = artifacts.require("MakerBadges");
 var BadgeFactory = artifacts.require("BadgeFactory");
+const kovan = require('./maker');
 
-module.exports = function(deployer) { 
-	deployer.deploy(MakerBadges).then(function() {
+
+module.exports = function(deployer) {
+	deployer.deploy(MakerBadges, kovan.maker.pot, kovan.maker.chief, kovan.maker.flipper).then(function() {
 		return deployer.deploy(BadgeFactory, MakerBadges.address);
 	});
 };
---
+++ migrations/maker.js
@@ -0,0 +1,14 @@
+// This file contains MakerDAO Kovan Addresses
+
+module.exports = {
+maker: {
+  // MCD_POT Kovan Address https://kovan.etherscan.io/address/0xea190dbdc7adf265260ec4da6e9675fd4f5a78bb#code
+  pot: '0xEA190DBDC7adF265260ec4dA6e9675Fd4f5A78bb',
+
+  // MCD_ADM Kovan Address https://kovan.etherscan.io/address/0xbBFFC76e94B34F72D96D054b31f6424249c1337d#code
+  chief: '0xbBFFC76e94B34F72D96D054b31f6424249c1337d',
+
+  // MCD_FLIP_ETH_A Kovan Address https://kovan.etherscan.io/address/0xB40139Ea36D35d0C9F6a2e62601B616F1FfbBD1b#code	
+  flipper: '0xB40139Ea36D35d0C9F6a2e62601B616F1FfbBD1b',
+}
+};
