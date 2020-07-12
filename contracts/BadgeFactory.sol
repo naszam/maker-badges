38a6eb289c251f4bcfdc6bb095de0faac2bd2fc5
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-07-12 21:04:50 +0100

    update to solc v0.6.11

--- contracts/BadgeFactory.sol
+++ contracts/BadgeFactory.sol
@@ -1,5 +1,5 @@
 /// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.6.10;
+pragma solidity 0.6.11;
 
 /// @title Non-transferable Badges for Maker Ecosystem Activity, CDIP 18
 /// @author Nazzareno Massari @naszam
