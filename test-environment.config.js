468dc78fbd536e2ade9ff714156ce9904f9671d5
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-08-25 15:09:47 +0100

    add infura mainnet api key to tests config

--- test-environment.config.js
+++ test-environment.config.js
@@ -1,5 +1,9 @@
 // test-environment.config.js
 
+const { config } = require("./config.js");
+
+var key = config.INFURA_API_KEY;
+
 module.exports = {
  accounts: {
    amount: 10, // Number of unlocked accounts
@@ -17,6 +21,8 @@ module.exports = {
 
  node: { // Options passed directly to Ganache client
    gasLimit: 8e6, // Maximum gas per block
-   gasPrice: 20e9 // Sets the default gas price for transactions if not otherwise specified.
+   gasPrice: 20e9, // Sets the default gas price for transactions if not otherwise specified.
+   fork: 'https://mainnet.infura.io/v3/' + key, // An url to Ethereum node to use as a source for a fork
+   //unlocked_accounts: [''], // Array of addresses specifying which accounts should be unlocked.
  },
 };
