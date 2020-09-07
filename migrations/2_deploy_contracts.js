7d02f943dbaee214239b7056e9d24d05e48cf535
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-09-07 18:16:06 +0100

    update migrations with trusted forwarder kovan address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 var MakerBadges = artifacts.require("MakerBadges");
 var BadgeFactory = artifacts.require("BadgeFactory");
-const kovan = require('./maker');
+const kovan = require('./kovan');
 
 
 module.exports = function(deployer) {
-	deployer.deploy(MakerBadges, kovan.maker.chai, kovan.maker.chief, kovan.maker.flipper).then(function() {
+	deployer.deploy(MakerBadges, kovan.opengsn.trustedForwarder, kovan.maker.chai, kovan.maker.chief, kovan.maker.flipper).then(function() {
 		return deployer.deploy(BadgeFactory, MakerBadges.address);
 	});
 };
