9dbd9b12a5f034f27381da1343c4096a2471ab88
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-09-17 15:54:42 +0100

    add badge paymaster to migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var MakerBadges = artifacts.require("MakerBadges");
 var BadgeFactory = artifacts.require("BadgeFactory");
+var BadgePaymaster = artifacts.require("BadgePaymaster");
 const kovan = require('./kovan');
 
 
@@ -7,4 +8,6 @@ module.exports = function(deployer) {
 	deployer.deploy(MakerBadges, kovan.opengsn.trustedForwarder, kovan.maker.chai, kovan.maker.chief, kovan.maker.flipper).then(function() {
 		return deployer.deploy(BadgeFactory, kovan.opengsn.trustedForwarder, MakerBadges.address);
 	});
+
+	deployer.deploy(BadgePaymaster);
 };
