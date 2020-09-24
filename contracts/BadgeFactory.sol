c24bb2c4af2d7ce5774da96089390ff7b97af019
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-09-24 14:28:44 +0100

    update SPDX licence identifier to AGPL-3.0-only

--- contracts/BadgeFactory.sol
+++ contracts/BadgeFactory.sol
@@ -1,4 +1,4 @@
-/// SPDX-License-Identifier: AGPL-3.0
+/// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.6.12;
 
 /// @title Non-transferable Badges for Maker Ecosystem Activity, CDIP 18
