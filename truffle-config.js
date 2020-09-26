488eb0b3af8cf92b225583c7836815d049b33976
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-09-27 00:36:14 +0100

    add 50 gwei gas price to devnet on truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -48,6 +48,7 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+      gasPrice: "50000000000", // 50 Gwei
      },
 
      kovan: {
