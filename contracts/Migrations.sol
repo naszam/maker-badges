84bf883b5f19fc2297640d9723c42502884a3a1e
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-10-18 18:20:09 +0100

    upgrade migrations contract to last truffle version

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,19 +1,19 @@
-/// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.6.12;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
 
 contract Migrations {
-    address public owner;
-    uint public last_completed_migration;
+  address public owner = msg.sender;
+  uint public last_completed_migration;
 
-    constructor() public {
-        owner = msg.sender;
-    }
+  modifier restricted() {
+    require(
+      msg.sender == owner,
+      "This function is restricted to the contract's owner"
+    );
+    _;
+  }
 
-    modifier restricted() {
-        if (msg.sender == owner) _;
-    }
-
-    function setCompleted(uint completed) public restricted {
-        last_completed_migration = completed;
-    }
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
 }
