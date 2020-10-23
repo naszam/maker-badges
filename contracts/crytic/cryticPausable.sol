d2c58489aa1e950bbd0e23f9510085e4b8900474
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-10-23 18:19:04 +0100

    add echidna access control property

---
+++ contracts/crytic/cryticPausable.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity 0.6.12;
+
+import "../BadgeRoles.sol";
+
+contract CryticInterface{
+    address internal crytic_owner = address(0x41414141);
+    address internal crytic_user = address(0x42424242);
+    address internal crytic_attacker = address(0x43434343);
+}
+
+contract CryticTestBadgeRoles is CryticInterface, BadgeRoles {
+
+    constructor() public {
+        _setupRole(PAUSER_ROLE, crytic_owner);
+        paused();
+    }
+
+    function crytic_no_transfer() public view returns(bool){
+        return is_paused == true;
+    }
+
+}
