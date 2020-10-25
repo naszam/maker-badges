1de8907d6f30c790449e4ac33bd088309bcca96e
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-10-25 13:57:34 +0000

    update tests

--- test/BadgeRoles.test.js
+++ test/BadgeRoles.test.js
@@ -14,6 +14,7 @@ describe('BadgeRoles', function () {
 const [ owner, templater, random ] = accounts;
 
 const DEFAULT_ADMIN_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000';
+const ADMIN_ROLE = web3.utils.soliditySha3('ADMIN_ROLE');
 const TEMPLATER_ROLE = web3.utils.soliditySha3('TEMPLATER_ROLE');
 const PAUSER_ROLE = web3.utils.soliditySha3('PAUSER_ROLE');
 
@@ -32,6 +33,11 @@ const PAUSER_ROLE = web3.utils.soliditySha3('PAUSER_ROLE');
         expect(await roles.getRoleMember(DEFAULT_ADMIN_ROLE, 0)).to.equal(owner);
       });
 
+      it('owner has the admin role', async function () {
+        expect(await roles.getRoleMemberCount(ADMIN_ROLE)).to.be.bignumber.equal('1');
+        expect(await roles.getRoleMember(ADMIN_ROLE, 0)).to.equal(owner);
+      });
+
       it('owner has the templater role', async function () {
         expect(await roles.getRoleMemberCount(TEMPLATER_ROLE)).to.be.bignumber.equal('1');
         expect(await roles.getRoleMember(TEMPLATER_ROLE, 0)).to.equal(owner);
@@ -40,50 +46,6 @@ const PAUSER_ROLE = web3.utils.soliditySha3('PAUSER_ROLE');
       it('owner has the pauser role', async function () {
         expect(await roles.getRoleMemberCount(PAUSER_ROLE)).to.be.bignumber.equal('1');
         expect(await roles.getRoleMember(PAUSER_ROLE, 0)).to.equal(owner);
-      });
-  });
-
-  // Check addTemplater() for success when an admin is adding a new templater
-  // Check addTemplater() for sucessfully emit event when the templater is added
-  // Check addTemplater() for failure when a random address try to add a templater
-  describe('addTemplater()', async function () {
-
-      it('admin should be able to add a templater', async function () {
-        await roles.addTemplater(templater, { from: owner });
-        expect(await roles.hasRole(TEMPLATER_ROLE, templater)).to.equal(true);
-      });
-
-      it('should emit the appropriate event when a new templater is added', async function () {
-        const receipt = await roles.addTemplater(templater, { from: owner });
-        expectEvent(receipt, 'RoleGranted', { account: templater });
-      });
-
-      it('random address should not be able to add a new templater', async function () {
-        await expectRevert(roles.addTemplater(templater, { from: random }), 'BadgeFactory: caller is not an admin');
-      });
-  });
-
-  // Check removeTemplater() for success when an admin is removing a new templater
-  // Check removeTemplater() for sucessfully emit event when the templater is removed
-  // Check removeTemplater() for failure when a random address try to remove a templater
-  describe('removeTemplater()', async function () {
-
-      beforeEach(async function () {
-        await roles.addTemplater(templater, { from: owner });
-      });
-
-      it('admin should be able to remove a templater', async function () {
-        await roles.removeTemplater(templater, { from: owner });
-        expect(await roles.hasRole(TEMPLATER_ROLE, templater)).to.equal(false)
-      });
-
-      it('should emit the appropriate event when a templater is removed', async function () {
-        const receipt = await roles.removeTemplater(templater, { from: owner });
-        expectEvent(receipt, 'RoleRevoked', { account: templater });
-      });
-
-      it('random address should not be able to remove a templater', async function () {
-        await expectRevert(roles.removeTemplater(templater, { from: random }), 'BadgeFactory: caller is not an admin');
       });
   });
 
