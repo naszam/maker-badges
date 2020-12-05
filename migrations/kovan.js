a2cbbb7005aef0a0df09d24695ef76fb3486d804
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-12-05 18:11:41 +0000

    update migrations, chief kovan 1.2

--- migrations/kovan.js
+++ migrations/kovan.js
@@ -5,8 +5,8 @@ maker: {
   // CHAI Kovan Address https://kovan.etherscan.io/address/0x623f148dabd40dd221807c5e98235bc40e54a106#code
   chai: '0x623f148dAbD40dd221807c5e98235bC40E54a106',
 
-  // MCD_ADM Kovan Address https://kovan.etherscan.io/address/0xbBFFC76e94B34F72D96D054b31f6424249c1337d#code
-  chief: '0xbBFFC76e94B34F72D96D054b31f6424249c1337d',
+  // MCD_ADM Kovan Address https://kovan.etherscan.io/address/0x27E0c9567729Ea6e3241DE74B3dE499b7ddd3fe6#code
+  chief: '0x27E0c9567729Ea6e3241DE74B3dE499b7ddd3fe6',
 
   // MCD_FLIP_ETH_A Kovan Address https://kovan.etherscan.io/address/0x750295A8db0580F32355f97de7918fF538c818F1#code
   flipper: '0x750295A8db0580F32355f97de7918fF538c818F1',
