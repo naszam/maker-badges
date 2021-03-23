eb2744835327b072c8fc177b70db5d86b336d519
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2021-03-23 18:46:38 +0000

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config"
 
+import "@nomiclabs/hardhat-solhint"
 import "@nomiclabs/hardhat-ethers"
 import "@nomiclabs/hardhat-waffle"
 import "@nomiclabs/hardhat-web3"
