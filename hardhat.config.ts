036cd61dbc6ef4825eab30986bcc088d1b7b2939
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2021-04-11 16:17:02 +0100

    add deploy script + accounts, clean tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,7 @@
-import { task, HardhatUserConfig } from "hardhat/config"
+import { HardhatUserConfig } from "hardhat/config"
+import { NetworkUserConfig } from "hardhat/types"
+import "./tasks/accounts"
+import "./tasks/clean"
 
 import "@nomiclabs/hardhat-solhint"
 import "@nomiclabs/hardhat-ethers"
@@ -10,23 +13,47 @@ import "solidity-coverage"
 import "hardhat-gas-reporter"
 import "dotenv/config"
 
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners()
+const chainIds = {
+  hardhat: 31337,
+  sokol: 77,
+  xdai: 100,
+}
 
-  for (const account of accounts) {
-    console.log(await account.address)
+//if (!process.env.DEPLOYER_ADDRESS) throw new Error("Please set your DEPLOYER_ADDRESS in a .env file")
+
+// Define network configurations
+function createNetworkConfig(network: keyof typeof chainIds): NetworkUserConfig {
+  const url: string = "https://" + network + ".poa.network"
+  return {
+    accounts: {
+      count: 10,
+      initialIndex: 0,
+      mnemonic: process.env.MNEMONIC,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[network],
+    url,
   }
-})
+}
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.6.12",
-  },
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      blockGasLimit: 10000000,
+      chainId: chainIds.hardhat,
     },
     localhost: {},
+    sokol: createNetworkConfig("sokol"),
+    xdai: createNetworkConfig("xdai"),
+  },
+  solidity: {
+    version: "0.6.12",
+    settings: {
+      optimizer: {
+        enabled: false,
+        runs: 200,
+      },
+    },
   },
   gasReporter: {
     currency: "USD",
