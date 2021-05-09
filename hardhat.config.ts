b30c59b8f3f934cdbbff5362af1dc18f5a4694d0
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2021-05-09 23:23:03 +0100

    Enable optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -54,7 +54,7 @@ const config: HardhatUserConfig = {
     version: "0.8.0",
     settings: {
       optimizer: {
-        enabled: false,
+        enabled: true,
         runs: 200,
       },
     },
